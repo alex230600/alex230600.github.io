069c16afec6163d54561f6633bff56df707f3df7
Mosin Alexey <alexei_s3000@mail.ru>
2021-04-29 12:19:19 +0300

    4 задание

---
+++ js.js
@@ -0,0 +1,8 @@
+var secondname = " ";
+
+document.getElementById('btn').addEventListener("click", Rename);
+
+function Rename(){
+	secondname = document.getElementById('_secondname').value;
+	document.getElementById('secondname').innerText = "Фамилия: " + secondname;
+}
